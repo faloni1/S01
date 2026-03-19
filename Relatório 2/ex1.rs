fe60963ec1721cb25bd3bdfd796c3c172365bb2f
faloni <40303923+faloni1@users.noreply.github.com>
2026-03-19 17:45:08 -0300

    Create ex1.rs

---
+++ Relatório 2/ex1.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn verificar_senha(senha: &str) -> bool {
+    if senha.len() < 10 {
+        return false;
+    }
+
+    let mut contagem_numeros = 0;
+    let mut tem_maiuscula = false;
+
+    for c in senha.chars() {
+        if c.is_digit(10) {
+            contagem_numeros += 1;
+        }
+        if c.is_ascii_uppercase() {
+            tem_maiuscula = true;
+        }
+    }
+
+    contagem_numeros >= 2 && tem_maiuscula
+}
+
+fn main() {
+    loop {
+        println!("Digite uma senha:");
+        let mut senha = String::new();
+        io::stdin().read_line(&mut senha).expect("Falha ao ler entrada");
+        let senha = senha.trim();
+
+        if verificar_senha(senha) {
+            println!("A senha é válida, seja bem vindo!"); [cite: 40]
+            break;
+        } else {
+            println!("Senha inválida. Tente novamente.");
+        }
+    }
+}
