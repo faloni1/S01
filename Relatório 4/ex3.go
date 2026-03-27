d50f9781201c4b738187aa5bd95b49252708d7eb
faloni <40303923+faloni1@users.noreply.github.com>
2026-03-27 19:13:49 -0300

    Create ex3.go

---
+++ Relatório 4/ex3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) {
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", a)
+		a, b = b, a+b
+	}
+	fmt.Println()
+}
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+
+	fibonacci(n)
+}
