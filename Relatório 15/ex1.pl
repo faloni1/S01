9b7deb25924cd234c998fa07f39ca278ce2f7938
faloni <40303923+faloni1@users.noreply.github.com>
2026-06-26 17:47:07 -0300

    Update ex1.pl

--- Relatório 15/ex1.pl
+++ Relatório 15/ex1.pl
@@ -11,18 +11,18 @@ componente(cloaking_device, 6, defesa).
 componente(android_reparo, 4, suporte).
 componente(estacao_pesquisa, 10, pesquisa).
 
-precisa(propulsor_ion,      reator_fusao).
-precisa(dobra_espacial,     propulsor_ion).
-precisa(escudo_plasma,      reator_fusao).
-precisa(ia_navegacao,       propulsor_ion).
-precisa(ia_navegacao,       modulo_suporte_vida).
-precisa(canhao_laser,       ia_navegacao).
-precisa(canhao_laser,       propulsor_ion).
-precisa(torpedo_foton,      ia_navegacao).
+precisa(propulsor_ion, reator_fusao).
+precisa(dobra_espacial, propulsor_ion).
+precisa(escudo_plasma, reator_fusao).
+precisa(ia_navegacao, propulsor_ion).
+precisa(ia_navegacao, modulo_suporte_vida).
+precisa(canhao_laser, ia_navegacao).
+precisa(canhao_laser, propulsor_ion).
+precisa(torpedo_foton, ia_navegacao).
 precisa(scanner_deep_space, modulo_suporte_vida).
-precisa(android_reparo,     modulo_suporte_vida).
-precisa(estacao_pesquisa,   scanner_deep_space).
-precisa(estacao_pesquisa,   android_reparo).
+precisa(android_reparo, modulo_suporte_vida).
+precisa(estacao_pesquisa, scanner_deep_space).
+precisa(estacao_pesquisa, android_reparo).
 
 peca_critica(P) :-
     componente(P, E, _),
