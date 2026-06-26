5679e3ed8d39205803602061a45553fe62d0d1da
faloni <40303923+faloni1@users.noreply.github.com>
2026-06-26 17:48:50 -0300

    Update ex3.pl

--- Relatório 15/ex3.pl
+++ Relatório 15/ex3.pl
@@ -11,30 +11,30 @@ componente(cloaking_device, 6, defesa).
 componente(android_reparo, 4, suporte).
 componente(estacao_pesquisa, 10, pesquisa).
 
-precisa(propulsor_ion,      reator_fusao).
-precisa(dobra_espacial,     propulsor_ion).
-precisa(escudo_plasma,      reator_fusao).
-precisa(ia_navegacao,       propulsor_ion).
-precisa(ia_navegacao,       modulo_suporte_vida).
-precisa(canhao_laser,       ia_navegacao).
-precisa(canhao_laser,       propulsor_ion).
-precisa(torpedo_foton,      ia_navegacao).
+precisa(propulsor_ion, reator_fusao).
+precisa(dobra_espacial, propulsor_ion).
+precisa(escudo_plasma, reator_fusao).
+precisa(ia_navegacao, propulsor_ion).
+precisa(ia_navegacao, modulo_suporte_vida).
+precisa(canhao_laser, ia_navegacao).
+precisa(canhao_laser, propulsor_ion).
+precisa(torpedo_foton, ia_navegacao).
 precisa(scanner_deep_space, modulo_suporte_vida).
-precisa(android_reparo,     modulo_suporte_vida).
-precisa(estacao_pesquisa,   scanner_deep_space).
-precisa(estacao_pesquisa,   android_reparo).
+precisa(android_reparo, modulo_suporte_vida).
+precisa(estacao_pesquisa, scanner_deep_space).
+precisa(estacao_pesquisa, android_reparo).
 
-possui(hangar_alpha,   reator_fusao).
-possui(hangar_alpha,   modulo_suporte_vida).
-possui(hangar_beta,    reator_fusao).
-possui(hangar_beta,    propulsor_ion).
-possui(hangar_beta,    modulo_suporte_vida).
-possui(hangar_gamma,   reator_fusao).
-possui(hangar_gamma,   cloaking_device).
-possui(hangar_gamma,   scanner_deep_space).
-possui(hangar_delta,   modulo_suporte_vida).
-possui(hangar_delta,   android_reparo).
-possui(hangar_delta,   scanner_deep_space).
+possui(hangar_alpha, reator_fusao).
+possui(hangar_alpha, modulo_suporte_vida).
+possui(hangar_beta, reator_fusao).
+possui(hangar_beta, propulsor_ion).
+possui(hangar_beta, modulo_suporte_vida).
+possui(hangar_gamma, reator_fusao).
+possui(hangar_gamma, cloaking_device).
+possui(hangar_gamma, scanner_deep_space).
+possui(hangar_delta, modulo_suporte_vida).
+possui(hangar_delta, android_reparo).
+possui(hangar_delta, scanner_deep_space).
 possui(hangar_epsilon, propulsor_ion).
 possui(hangar_epsilon, dobra_espacial).
 possui(hangar_epsilon, cloaking_device).
